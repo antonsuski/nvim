613abdc06e36e2d97dcc63228076a990c3222bf5
anotn <antonsyski@gmail.com>
2025-07-17 15:52:03 +0300

    Added lsp-nvimconfig autoload

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-FILE='init.vim'
+FILE='init.lua'
+LSP_PLUGIN='nvim-lspconfig'
 UNIX=${HOME}/'.config/nvim'
 
 DIR=${UNIX}
+LSP_PLUGIN_DIR="${DIR}/pack/nvim/start/${LSP_PLUGIN}"
 TARGET_FILE="${DIR}/${FILE}"
 
 echo 'Deploying NeoVIM configuration ...'
@@ -13,13 +15,14 @@ if test -d ${DIR}; then
 	echo 'NeoVIM directory exists'
 else
 	echo 'There is no NeoVIM root directory'
+	echo 'Creating nvim config directory ...'
 	mkdir -pv ${DIR}
 fi
 
 
 if  test -s ${TARGET_FILE} ; then
 	echo 'Configuration already exists.'
-	echo "Making backup file: ${TARGET_FILE}.bak"
+	echo "Making backup file ... : ${TARGET_FILE}.bak"
 	cp ${TARGET_FILE} ${TARGET_FILE}.bak
 else
 	echo 'Configureation does not exists.'
@@ -27,3 +30,12 @@ else
 	cp "${FILE}" "${TARGET_FILE}"
 fi
 
+echo 'Checking already existed nvim-lspconfig plugins ...'
+
+if test -d ${LSP_PLUGIN_DIR}; then
+	echo 'The nvim-lspconfig plugin exists'
+else
+	echo 'There is no nvim-lspconfig'
+	echo 'Installing nvim-lspconfig ...'
+	git clone https://github.com/neovim/nvim-lspconfig ~/.config/nvim/pack/nvim/start/nvim-lspconfig
+fi
