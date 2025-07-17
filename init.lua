e41fe58c12e6f0d062f1cbcabf015309f62c804c
anotn <antonsyski@gmail.com>
2025-07-17 15:38:58 +0300

    Init file was changed with lua script

---
+++ init.lua
@@ -0,0 +1,11 @@
+vim.lsp.enable('clangd')
+
+vim.cmd("set ignorecase")
+vim.cmd("set list")
+vim.cmd("set scrolloff=4")
+vim.cmd("set cmdheight=3")
+vim.cmd("set number")
+vim.cmd("set iskeyword-=_")
+vim.cmd("filetype on")
+vim.cmd("filetype plugin indent on")
+
