47ca1a37e5f9cd9d2272ea153a32eec95e2a7c1e
anton <anton.syski@gmail.com>
2025-12-03 15:38:36 +0300

    Added hotkeys for meson
    Fixed some packs

--- config/init.lua
+++ config/init.lua
@@ -12,7 +12,6 @@ vim.g.mapleader = ' '
 
 vim.cmd("filetype on")
 vim.cmd("filetype plugin indent on")
-vim.cmd("set iskeyword-=_")
 
 vim.pack.add({
 	{
@@ -22,8 +21,36 @@ vim.pack.add({
 	{
 		src = 'https://github.com/nvim-telescope/telescope.nvim',
 		version = '0.1.x'
+	},
+	{
+		src = 'https://github.com/nvim-treesitter/nvim-treesitter',
+		version = 'master'
 	}
 })
+
+vim.keymap.set('n', '<leader>mc',
+function()
+	vim.cmd('!meson compile -C build');
+end
+,{noremap = true, silent = true, desc = 'Compile via meson'})
+
+vim.keymap.set('n', '<leader>ms',
+function()
+	vim.cmd('!meson setup --native-file native.ini build');
+end
+,{noremap = true, silent = true, desc = 'Setup via meson'})
+
+vim.keymap.set('n', '<leader>mr',
+function()
+	vim.cmd('!meson setup --reconfigure build');
+end
+,{noremap = true, silent = true, desc = 'Reconfigure via meson'})
+
+vim.keymap.set('n', '<leader>mf',
+function()
+	vim.cmd('!meson format -i -r -c meson.format');
+end
+,{noremap = true, silent = true, desc = 'Formatting meson'})
 
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
